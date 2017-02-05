cdd00a3d38b9bcc4818a3999e04a903859a4a9cd
venkteshv <venktesh.viswanathan@nationstarmail.com>
2017-02-05 20:55:28 +0530

    schemas created

--- db/migrate/20170205140630_create_orders.rb
+++ db/migrate/20170205140630_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       t.integer :user_id
+  t.references :user, index: true, foreign_key: true
      t.integer :total
      t.integer :order_number
       t.timestamps
