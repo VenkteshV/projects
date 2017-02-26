2e0ea3322d1e6955dc52503cf5b31c1f2ec5c902
venkteshv <venktesh.viswanathan@nationstarmail.com>
2017-02-27 00:17:08 +0530

    added rack middleware to eable cors

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require_relative 'boot'
 
+
+require 'rails/all'
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -26,5 +29,11 @@ module Proj
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+  allow do
+    origins '*'
+    resource '*', :headers => :any, :methods => [:get, :post, :options]
+  end
+end
   end
 end
